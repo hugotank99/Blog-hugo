12eceb10068f7f41540c0a2af7100e4b9fba01c8
hugotank99 <hugoramosjan@gmail.com>
2022-12-05 12:49:19 -0300

    update

---
+++ blog-pessoal/src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import  "./Home.css";
+
+function Home(){
+    return(
+        <div>
+            <h1>Pag Home</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlezqmtwOYwdI7Yq59Jx7K_4WabNAfrAKTPw&usqp=CAU" alt="Fases da Demi Lovato" ></img> 
+        </div>
+    );
+}
+export default Home;

f750a40cdb6319a25be0594d00f3b257a2e7422a
hugotank99 <hugoramosjan@gmail.com>
2022-12-08 12:28:35 -0300

    updates

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Grid } from '@material-ui/core';
+import Home from './pages/home/Home';
+import Footer from './componetes/estaticos/footer/Footer';
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
+import Navbar from './componetes/estaticos/navBar/NavBar';
+
 
 function App() {
-  return (<div></div>);
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Home/>
+      </Routes>
+      <Footer />
+    </Router>
+
+  );
 }
 
 export default App;
--- blog-pessoal/src/pages/home/Home.tsx
+++ blog-pessoal/src/pages/home/Home.tsx
@@ -1,12 +1,31 @@
-import React from "react";
-import  "./Home.css";
+import React from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from '@mui/material';
+import './Home.css';
 
-function Home(){
-    return(
-        <div>
-            <h1>Pag Home</h1>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlezqmtwOYwdI7Yq59Jx7K_4WabNAfrAKTPw&usqp=CAU" alt="Fases da Demi Lovato" ></img> 
-        </div>
+function Home() { 
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "Black" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vinde!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#00a152", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+        </>
     );
 }
+
 export default Home;
